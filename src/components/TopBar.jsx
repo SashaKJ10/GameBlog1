53267adb3050b1b79b9ddaec99d9df1f0e2f8ad7
Dmytro Palamarchuk <dmytro.palamarchuk@cleverbridge.com>
2023-01-10 22:18:29 +0200

    Adjusted TopBar a bit

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { GiAnts } from 'react-icons/gi';
+import { GiSwordSlice } from 'react-icons/gi';
 function TopBar({ signedIn, userInfo, setSignedIn, games }) {
   let [clickInfo, setClickInfo] = useState({
     gameButtonClicked: false,
@@ -11,9 +11,9 @@ function TopBar({ signedIn, userInfo, setSignedIn, games }) {
 
   const classes = {
     button:
-      'bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-2 rounded cursor-pointer',
+      'bg-green-500/75 whitespace-nowrap hover:bg-green-600/75 text-white font-bold py-2 px-2 rounded cursor-pointer',
     buttonClicked:
-      'bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-2 rounded cursor-pointer',
+      'bg-blue-500/75 whitespace-nowrap hover:bg-blue-600/75 text-white font-bold py-2 px-2 rounded cursor-pointer',
   };
   let admin = localStorage.getItem('admin');
   let signedInUser = localStorage.getItem('user');
@@ -38,14 +38,14 @@ function TopBar({ signedIn, userInfo, setSignedIn, games }) {
   };
   return (
     <div className="fixed top-0 flex w-full bg-gray-500 shadow border-gray-300">
-      <div className="flex items-center justify-center">
-        <GiAnts className="h-10 w-10" />
+      <div className="cursor-default flex items-center mx-2 my-1 px-2 py-1 rounded-lg border-2
+                      border-t-emerald-300 border-r-red-300 border-b-blue-300 border-l-pink-300">
+        <GiSwordSlice className="w-10 h-10 text-white"/>
+        <span className="ml-1 text-white text-xl">GameHub</span>
       </div>
-      <div className="text-white-600/100 p-2 text-xl mt-1.5">GameHub</div>
-      <div className="border-l-4 border-indigo-500 border-1 h-25"></div>
       <div className="flex w-full justify-between">
-        <div className="flex ">
-          <div className="px-2 mt-3 mb-1">
+        <div className="flex items-center">
+          <div className="px-2">
             {signedInUser ? (
               <Link
                 to="/account"
@@ -65,7 +65,7 @@ function TopBar({ signedIn, userInfo, setSignedIn, games }) {
               </Link>
             ) : null}
           </div>
-          <div className="px-2 mt-3 mb-1">
+          <div className="px-2">
             <Link
               to="/"
               onClick={(e) =>
@@ -85,15 +85,15 @@ function TopBar({ signedIn, userInfo, setSignedIn, games }) {
           </div>
         </div>
 
-        <form class="flex items-center">
+        <form className="flex items-center w-1/2">
           <label for="simple-search" class="sr-only">
             Search
           </label>
-          <div class="relative w-full">
-            <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+          <div className="relative w-full mr-2">
+            <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
               <svg
                 aria-hidden="true"
-                class="w-5 h-5 text-gray-500 dark:text-gray-400"
+                className="w-5 h-5 text-gray-500 dark:text-gray-400"
                 fill="currentColor"
                 viewBox="0 0 20 20"
                 xmlns="http://www.w3.org/2000/svg"
@@ -108,17 +108,17 @@ function TopBar({ signedIn, userInfo, setSignedIn, games }) {
             <input
               type="text"
               id="simple-search"
-              class="bg-gray-50 border w-full border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              className="bg-gray-50 border w-full border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               placeholder="Search"
               required
             />
           </div>
           <button
             type="submit"
-            class="p-2.5 ml-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            className={classes.button}
           >
             <svg
-              class="w-5 h-5"
+                className="w-5 h-5"
               fill="none"
               stroke="currentColor"
               viewBox="0 0 24 24"
@@ -131,12 +131,12 @@ function TopBar({ signedIn, userInfo, setSignedIn, games }) {
                 d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
               ></path>
             </svg>
-            <span class="sr-only">Search</span>
+            <span className="sr-only">Search</span>
           </button>
         </form>
 
-        <div className="flex">
-          <div className="px-2 mt-3 mb-1">
+        <div className="flex items-center">
+          <div className="px-2">
             {signedUserInfo?.email === admin && signedInUser ? (
               <Link
                 to="/editing"
@@ -156,9 +156,9 @@ function TopBar({ signedIn, userInfo, setSignedIn, games }) {
               </Link>
             ) : null}
           </div>
-          <div className="px-2 mt-3 mb-1">
+          <div className="px-2">
             {signedInUser ? (
-              <h2 className="flex justify-content items-center text-bold ">
+              <h2 className="cursor-default flex justify-content items-center text-bold text-white whitespace-nowrap">
                 {signedUserInfo?.email}
               </h2>
             ) : (
@@ -180,7 +180,7 @@ function TopBar({ signedIn, userInfo, setSignedIn, games }) {
               </Link>
             )}
           </div>
-          <div className="px-2 mt-3 mb-1">
+          <div className="px-2">
             {signedInUser ? (
               <button className={classes.button} onClick={(e) => LogOut(e)}>
                 Log out
