7fc2761b450655e27fec93abaac5a14f85fe8c62
SashaKJ10 <jkaeasadbalanyuk54@gmail.com>
2023-01-31 23:18:54 +0200

    Bug's fixed

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,7 +1,14 @@
 import React, { useState, useCallback, useEffect } from 'react';
-function Comment({ comment, showReplyInput, setShowReplyInput }) {
+function Comment({
+  comment,
+  showReplyInput,
+  setShowReplyInput,
+  deleteComment,
+}) {
   const [reply, setReply] = useState('');
   const [replies, setReplies] = useState([]);
+  const [replyId, setReplyId] = useState(0);
+  const admin = localStorage.getItem('admin');
   useEffect(() => {
     const repliesFromLocalStorage = JSON.parse(
       localStorage.getItem(`replies-${comment.id}-${window.location.pathname}`)
@@ -22,9 +29,10 @@ function Comment({ comment, showReplyInput, setShowReplyInput }) {
   const handleSubmit = useCallback(
     (event) => {
       event.preventDefault();
-      setReplies([...replies, reply]);
+      setReplies([...replies, { replyId: replyId, reply: reply }]);
       setReply('');
       setShowReplyInput(false);
+      setReplyId(replyId++);
     },
     [replies, reply]
   );
@@ -33,14 +41,22 @@ function Comment({ comment, showReplyInput, setShowReplyInput }) {
     <div classNme="p-4 bg-gray-300 rounded-lg shadow-md">
       <p className="text-gray-700 font-medium mb-2">Comment:</p>
       <p className="text-gray-700 text-xl mb-4">{comment.text}</p>
+      {admin ? (
+        <button
+          className="bg-gray-400 rounded p-2 ml-2"
+          onClick={() => deleteComment(comment.id)}
+        >
+          Delete
+        </button>
+      ) : null}
       {replies.map((reply, index) => (
         <div
-          key={Date.now()}
+          key={reply.replyId}
           className="p-4 bg-gray-200 rounded-lg shadow-sm mt-4"
         >
           <p className="text-gray-700 font-medium mb-2">Reply:</p>
           <p key={index} className="text-gray-500 pl-4">
-            {reply}
+            {reply.reply}
           </p>
         </div>
       ))}
@@ -52,8 +68,12 @@ function Comment({ comment, showReplyInput, setShowReplyInput }) {
             onChange={(event) => setReply(event.target.value)}
             className="border border-gray-400 rounded p-2 mb-2"
           />
-          <button type="submit" className="bg-gray-400 rounded p-2 ml-2">
-            Submit Reply
+          <button
+            type="submit"
+            onClick={handleSubmit}
+            className="bg-gray-400 rounded p-2 ml-2"
+          >
+            Submit Your Reply
           </button>
         </form>
       )}
